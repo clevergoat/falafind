4d552d272df34a49caedbb86c4eb06676649bf3d
Rikard Andresen <GWI-Mac@Rikards-MacBook-Pro.local>
2013-11-27 17:17:34 +0000

    trying heroku again

--- app/helpers/shops_helper.rb
+++ app/helpers/shops_helper.rb
@@ -3,4 +3,11 @@ module ShopsHelper
 	def walking_speed(shop, query)
 
 		@distance = shop.distance_to(query)
+
+		# the average walking speed is 5 km/hr
+		# the average walking speed is 0.08333 km/min
+
+		(@distance / 0.0833).round(0)
+	end
+
 end
